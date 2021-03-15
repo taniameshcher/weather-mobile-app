86de0f234bcae866cb6ba53f4f4af0f5676b77cd
Stephanie220988 <77506795+Stephanie220988@users.noreply.github.com>
2021-03-15 18:18:51 +0100

    Added Time and Temperature

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -6,109 +6,66 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Button, Text, View} from 'react-native';
+import moment from 'moment';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default App = () => {
+  const [data, setData] = useState('Mood');
+  const [isLoading, setLoading] = useState(true);
 
-const App: () => React$Node = () => {
+  function roundTemp(temp) {
+    const roundTemp = parseFloat(temp).toFixed(2);
+    return roundTemp;
+  }
+
+  function getUtcTime(date) {
+    const datum = new Date(date * 1000);
+    var utcTime = moment(datum.toUTCString());
+    var offsetTime = utcTime.utcOffset(1).format('dd, DD.MM.YYYY HH:mm');
+    return offsetTime;
+  }
+
+  useEffect(() => {
+    this.fetchData();
+  }, []);
+
+  fetchData = () => {
+    fetch(
+      'https://6xt8gjjija.execute-api.eu-west-1.amazonaws.com/default/return_json',
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setData({
+          Temperatur: roundTemp(json.records[0].MeasureValue),
+          Datum: getUtcTime(json.common_attrs.Time),
+        });
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  };
+
+  handlePress = () => {
+    // alert('Simple Button pressed');
+    this.fetchData();
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One HALLO </Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View>
+      {isLoading ? (
+        <Text>Data is Loading</Text>
+      ) : (
+        <React.Fragment>
+          <Text>{data.Temperatur}</Text>
+          <Text>{data.Datum}</Text>
+          <Button
+            title="Temperature outside"
+            onPress={this.handleOutsideTemp}></Button>
+          <Button
+            title="Temperature inside"
+            onPress={this.handleInnerTemp}></Button>
+        </React.Fragment>
+      )}
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
